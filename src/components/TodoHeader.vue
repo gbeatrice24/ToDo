795537429f2d1b67ebd6f6841eccaaf0c1b09ae2
gbeatrice24 <gligorbeatrice@icloud.com>
2025-07-15 13:48:57 +0300

    ITL-1 Implement Page header - fixes2

--- src/components/TodoHeader.vue
+++ src/components/TodoHeader.vue
@@ -1,3 +1,16 @@
+<template>
+    <div class="flex justify-center items-center h-screen">
+        <div class="flex items-center space-x-4">
+            <h1 class="text-4xl font-semibold text-black">To do list</h1>
+            <button 
+                class="flex items-center justify-center bg-green-400 text-white rounded-full w-10 h-10" 
+                @click="createTodo">
+                <font-awesome-icon :icon="['fas', 'plus']" />
+            </button>
+        </div>
+    </div>
+</template>
+
 <script setup lang="ts">
 import { ref } from 'vue'
 
@@ -20,19 +33,6 @@ function createTodo() {
 }
 
 </script>
-
-<template>
-    <div class="flex justify-center items-center h-screen">
-        <div class="flex items-center space-x-4">
-            <h1 class="text-4xl font-semibold text-black">To do list</h1>
-            <button 
-                class="flex items-center justify-center bg-green-400 text-white rounded-full w-10 h-10" 
-                @click="createTodo">
-                <font-awesome-icon :icon="['fas', 'plus']" />
-            </button>
-        </div>
-    </div>
-</template>
 
 <style>
 </style>
