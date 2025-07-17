0c7decd4528f8f9ea4042a9d5c8b316ab7c68e6f
gbeatrice24 <gligorbeatrice@icloud.com>
2025-07-17 09:23:24 +0300

    ITL-2 Mobile friendly looking

--- src/layout/MainLayout.vue
+++ src/layout/MainLayout.vue
@@ -1,17 +1,14 @@
 <template>
     <div class="min-h-screen space-y-15">
-        <TodoHeader @add-task="handleAddTask"/>
+        <TodoHeader @add-task="handleAddTask" />
 
         <div v-if="isEmpty" class="flex justify-center items-center">
             <img src="../assets/notodos.png" alt="NoTodos" class="max-w-xs mx-auto">
         </div>
         <div v-else class="flex justify-center items-center">
             <div class="task-list grid gap-8">
-                <TaskElement 
-                    v-for="task in tasks" 
-                    :key="task.id" 
-                    :task="task" 
-                />
+                <component v-for="task in tasks" :is="isMobile ? MobileTaskCard : DesktopTaskCard" :key="task.id"
+                    :task="task" @done-clicked="handleDoneClicked" />
             </div>
         </div>
 
@@ -20,16 +17,24 @@
 
 <script setup lang="ts">
 import TodoHeader from './TodoHeader.vue';
-import TaskElement from '@/components/TaskElement.vue';
+import DesktopTaskCard from '@/components/DesktopTaskCard.vue';
+import MobileTaskCard from '@/components/MobileTaskCard.vue';
+
 import { Task } from '@/types/Task';
-import { ref, computed } from 'vue'
+
+import { ref, computed, reactive } from 'vue'
+
+import { useWindowSize } from '@vueuse/core'
 
 const tasks = ref<Task[]>([])
 let nextId = 0
 const isEmpty = computed(() => tasks.value.length == 0)
 
-function handleAddTask(){
-    const task = ({
+const { width } = useWindowSize()
+const isMobile = computed(() => width.value < 768)
+
+function handleAddTask() {
+    const task = reactive({
         id: ++nextId,
         name: `Task ${nextId}`,
         desc: `This is a description`,
@@ -42,38 +47,49 @@ function handleAddTask(){
     console.log('new task added:', task)
 }
 
+function handleDoneClicked(id: number) {
+    tasks.value = tasks.value.map((task) => {
+        if (task.id === id) {
+            task.done = !task.done
+        }
+        return { ...task }
+    })
+
+    console.log("Task", id, "done button clicked")
+}
+
 
 
 //////// TEST /////////
-    const task1 = ({
-        id: ++nextId,
-        name: `Task ${nextId}`,
-        desc: `This is a description`,
-        priority: 'High',
-        done: false,
-        date: new Date().toLocaleDateString(),
-    })
-    tasks.value.push(task1)
+const task1 = ({
+    id: ++nextId,
+    name: `Task ${nextId}`,
+    desc: `This is a description`,
+    priority: 'High',
+    done: false,
+    date: new Date().toLocaleDateString(),
+})
+tasks.value.push(task1)
 
-    const task2 = ({
-        id: ++nextId,
-        name: `Task ${nextId}`,
-        desc: `This is a description`,
-        priority: 'Medium',
-        done: false,
-        date: new Date().toLocaleDateString(),
-    })
-    tasks.value.push(task2)
+const task2 = ({
+    id: ++nextId,
+    name: `Task ${nextId}`,
+    desc: `This is a description`,
+    priority: 'Medium',
+    done: false,
+    date: new Date().toLocaleDateString(),
+})
+tasks.value.push(task2)
 
-    const task3 = ({
-        id: ++nextId,
-        name: `Task ${nextId}`,
-        desc: `This is a description`,
-        priority: 'Low',
-        done: false,
-        date: new Date().toLocaleDateString(),
-    })
-    tasks.value.push(task3)
+const task3 = ({
+    id: ++nextId,
+    name: `Task ${nextId}`,
+    desc: `This is a description`,
+    priority: 'Low',
+    done: false,
+    date: new Date().toLocaleDateString(),
+})
+tasks.value.push(task3)
 
 
 </script>
