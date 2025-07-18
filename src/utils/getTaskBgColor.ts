379693b56e7e6697ca81aa4335284d97fe5fd1ea
gbeatrice24 <gligorbeatrice@icloud.com>
2025-07-18 16:53:28 +0300

    ITL-3 Subtask 4 :After tapping the task, Card will convert it into edit mode

--- src/utils/getTaskBgColor.ts
+++ src/utils/getTaskBgColor.ts
@@ -1,20 +1,18 @@
 import { Task } from '@/types/Task';
 import { computed } from 'vue';
 
-export function getTaskBgColor(task: Task) {
-    const bgColor = computed(() => {
-        switch (task.priority) {
-            case 'High':
-                return 'bg-red-500'
-            case 'Medium':
-                return 'bg-amber-500'
-            case 'Low':
-                return 'bg-teal-400'
-            default:
-                return 'bg-gray-500'
-        }
-    })
-
-    return bgColor
-
+export function getTaskBgColor(task: Task): string {
+    switch (task.priority) {
+        case 'High':
+            return 'bg-red-500'
+        case 'Medium':
+            return 'bg-amber-500'
+        case 'Low':
+            return 'bg-teal-400'
+        default:
+            return 'bg-gray-500'
+    }
 }
+
+
+
