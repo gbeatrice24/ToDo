4a8077a0e55cac19f564a4d29f4f4e409759de3e
gbeatrice24 <gligorbeatrice@icloud.com>
2025-07-21 17:31:11 +0300

    ITL-3:  Subtask 6 & 7:  Make the design match with figma &Pop-up confirmation

--- src/components/TaskCard.vue
+++ src/components/TaskCard.vue
@@ -18,14 +18,16 @@
             <!-- task info -->
             <div :class="isMobile ? 'order-2 pl-2 w-7/8' : 'order-1 w-1/1'">
                 <div class="flex flex-row space-x-10 static">
-                    <input v-model="task.name" class="font-semibold text-black text-2xl outline-0 w-2/3">
+                    <!-- task name -->
+                    <input v-model="newName" class="font-semibold text-black text-2xl outline-0 w-2/3"
+                        :class="isMobile ? 'w-45' : ''">
 
                     <!-- task priority on desktop -->
                     <div v-if="!isMobile" class="relative">
                         <button @click="priorChooser = !priorChooser"
                             class="relative flex items-center justify-center rounded-full order-3 w-20"
                             :class="priorChooser ? 'bg-white text-black border-2' : [bgColor, 'text-white']">
-                            {{ task.priority }}
+                            {{ newPriority }}
 
                             <div v-if="task.editing">
                                 <img v-if="!priorChooser" src="@/assets/arrowDown.png" class="w-3 pl-1">
@@ -38,21 +40,17 @@
                             class="absolute top-full left-0 mt-1 w-full bg-white rounded-2xl border-2 z-10">
                             <ul class="flex flex-col text-sm text-black">
                                 <li v-for="(priority, index) in priorities"
-                                    @click="$emit('priority-chosen', { id: task.id, prio: priority.level }); priorChooser = false"
+                                    @click="newPriority = priority.level; priorChooser = false"
                                     class="px-2 hover:bg-gray-100" :class="[index === 0 ? 'rounded-t-2xl pt-2' : '',
                                     index === 2 ? 'rounded-b-2xl pb-2' : '']">{{ priority.level }}
                                 </li>
                             </ul>
                         </div>
-
-                        <!--  <div class="absolute inset-0 bg-white pointer-events-none transition-opacity"
-                            :class="priorChooser ? 'opacity-25' : 'opacity-0'"></div> -->
                     </div>
 
                     <!-- task priority on phone -> while being actively edited -->
-                    <div v-if="isMobile && task.editing" class="flex flex-row order-3 space-x-2">
-                        <button v-for="priority in priorities"
-                            @click="$emit('priority-chosen', { id: task.id, prio: priority.level })"
+                    <div v-if="isMobile && task.editing" class="flex flex-row order-3 space-x-2 ">
+                        <button v-for="priority in priorities" @click="newPriority = priority.level"
                             class="rounded-full order-3 h-3 w-3 focus:outline-2"
                             :class="getBgColor(priority.level)"></button>
                     </div>
@@ -65,7 +63,7 @@
                             <p class="w-xs" :class="task.editing ? '' : 'text-gray-500'">{{ task.date }}</p>
                         </div>
                         <!-- resize on bigger text? -->
-                        <textarea v-if="!isMobile || task.editing" v-model="text"
+                        <textarea v-if="!isMobile || task.editing" v-model="newDesc"
                             placeholder="Here goes the description." class=" w-90 outline-0 resize-none "
                             :class="task.editing ? 'h-20' : 'h-7'"></textarea>
                     </div>
@@ -82,11 +80,28 @@
                 </div>
 
                 <div v-if="task.editing" @click.stop class="space-x-3">
-                    <button @click="$emit('save-clicked', { id: task.id, newDesc: newDesc, newPrio: priority })"
+                    <button
+                        @click="$emit('save-clicked', { id: task.id, newName: newName, newDesc: newDesc, newPriority: newPriority })"
                         class="bg-emerald-400 text-white text-xs"
                         :class="isMobile ? 'font-semibold rounded-lg p-2 w-15' : 'rounded-xl p-3 w-20'">Save</button>
-                    <button @click="$emit('delete-clicked', task.id)" class="bg-gray-300 text-black text-xs w-20"
+                    <!--  <button @click="$emit('delete-clicked', task.id)" class="bg-gray-300 text-black text-xs w-20" -->
+                    <button @click="delPopupShowing = true" class="bg-gray-300 text-black text-xs w-20"
                         :class="isMobile ? 'font-semibold rounded-lg p-2 w-15' : 'rounded-xl p-3 w-20'">Delete</button>
+
+                    <!-- delete popup confirmation -->
+                    <div v-if="delPopupShowing" class="absolute z-50 flex items-center justify-center">
+                        <div v-if="delPopupShowing"
+                            class=" border p-4 rounded-xl shadow-lg bg-white space-y-4  space-x-3 text-center z-50">
+                            <h3 class="font-semibold">Are you sure you want to delete this item? This operation is
+                                permanent and you will not be able to undo this action!</h3>
+                            <button @click="$emit('delete-clicked', task.id)"
+                                class="font-semibold bg-red-500 text-white w-15 "
+                                :class="isMobile ? 'font-semibold rounded-lg p-2' : 'rounded-xl h-10'">Yes</button>
+                            <button @click="delPopupShowing = false" class="font-semibold bg-gray-300 text-black w-15"
+                                :class="isMobile ? 'rounded-lg p-2' : 'rounded-xl h-10'">Cancel</button>
+                        </div>
+                    </div>
+
                 </div>
             </div>
 
@@ -98,35 +113,51 @@
 
 <script setup lang="ts">
 import { Task } from '@/types/Task';
-import { getBgColor, getTaskBgColor } from '@/utils/getTaskBgColor';
+import { getBgColor } from '@/utils/getTaskBgColor';
 import { useWindowSize } from '@vueuse/core'
-import { ref, computed } from 'vue'
+import { ref, computed, watch } from 'vue'
 
 import { faCalendarDays } from '@fortawesome/free-solid-svg-icons'
 import { library } from '@fortawesome/fontawesome-svg-core'
+
 library.add(faCalendarDays)
 
 defineEmits<{
     (e: 'done-clicked', id: number): void,
     (e: 'edit-requested', id: number): void,
 
-    (e: 'save-clicked', payload: { id: number; newDesc: string, newPrio: string }): void,
+    (e: 'save-clicked', payload: { id: number; newName: string; newDesc: string, newPriority: string }): void,
     (e: 'delete-clicked', id: number): void,
 
     (e: 'priority-chosen', payload: { id: number, prio: string }): void
 }>()
 
 const props = defineProps<{ task: Task }>()
-const bgColor = computed(() => getTaskBgColor(props.task))
-const name = ref(props.task.name)
-const newDesc = ref('')
-const text = ref(props.task.desc)
-const priority = ref(props.task.priority)
+
+let newName = ref(props.task.name)
+let newDesc = ref(props.task.desc)
+let newPriority = ref(props.task.priority)
+
+const bgColor = computed(() => getBgColor(newPriority.value))
+const delPopupShowing = ref(false)
 
 const priorChooser = ref(false)
 const priorities = ref([{ level: 'Low' }, { level: 'Medium' }, { level: 'High' }])
 
 const { width } = useWindowSize()
 const isMobile = computed(() => width.value < 768)
+
+watch(() => props.task.editing, (editing) => {
+    delPopupShowing.value = false
+    if (!editing) resetTaskCard()
+})
+
+function resetTaskCard() {
+    newName.value = props.task.name
+    newDesc.value = props.task.desc
+    newPriority.value = props.task.priority
+
+    console.log("task card reseted", props.task.id)
+}
 
 </script>
--- src/utils/getTaskBgColor.ts
+++ src/utils/getTaskBgColor.ts
@@ -1,18 +1,3 @@
-import { Task } from '@/types/Task';
-
-export function getTaskBgColor(task: Task): string {
-    switch (task.priority) {
-        case 'High':
-            return 'bg-red-500'
-        case 'Medium':
-            return 'bg-amber-500'
-        case 'Low':
-            return 'bg-teal-400'
-        default:
-            return 'bg-gray-500'
-    }
-}
-
 export function getBgColor(priority: string): string {
     switch (priority) {
         case 'High':
